6cbdff14e1c8c8376b1af92829f30ef330aede51
Marcin Libelt <m.libelt@alekseon.com>
2023-06-14 20:59:13 +0200

    added Design Token from Figma Plugin, created transpilator function to translate bare token object to Tailwind config format

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,32 @@
 /** @type {import('tailwindcss').Config} */
+
+const tokens = require("./design-tokens/tokens");
+const transpile = require("./design-tokens/token-to-tailwind");
+
+const tailwindPropsNameMap = new Map([["fontWeights", "fontWeight"]]);
+
+const classes = {
+  colors: transpile(tokens.colors),
+  opacity: transpile(tokens.opacity),
+  fontWeight: transpile(tokens.fontWeights),
+  borderRadius: transpile(tokens.borderRadius),
+};
+
 module.exports = {
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
+    ...classes,
     extend: {
       backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic":
+          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
     },
   },
   plugins: [],
-}
+};
