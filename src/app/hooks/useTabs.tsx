559ac3dccd2eb1b7e94fd2c5b70d3ed6c9ae80a4
Marcin Libelt <m.libelt@alekseon.com>
2023-06-22 23:16:52 +0200

    cleaning up ts errors

--- src/app/hooks/useTabs.tsx
+++ src/app/hooks/useTabs.tsx
@@ -1,6 +1,9 @@
-export default function useTabs(group: {}, renderer: object) {
+export default function useTabs(
+  group: { [key: string]: object | any },
+  renderer: any
+) {
   const list: string[] = [],
-    panels: object[] = [];
+    panels: JSX.Element[] = [];
 
   Object.keys(group).map((key) => {
     list.push(key);
